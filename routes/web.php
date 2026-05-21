e38b281ae46c6487ef52b25704ce1b2f53e21c37
aroskaa <divaaroska10@gmail.com>
2026-05-21 22:14:36 +0800

    feat: base routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,10 +12,27 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', DashboardController::class)
+        ->name('dashboard');
+
+    Route::middleware('role:owner,admin')->group(function () {
+        Route::get('/admin/products', function () {
+            return view('pages.products.index');
+        })->name('products.index');
+    });
+
+    Route::middleware('role:owner,admin,cashier')->group(function () {
+        Route::get('/pos', function () {
+            return view('pages.pos.index');
+        })->name('pos.index');
+    });
+
+    Route::middleware('role:owner,admin')->group(function () {
+        Route::get('/reports', function () {
+            return view('pages.reports.index');
+        })->name('reports.index');
+    });
 });
 
 require __DIR__.'/auth.php';
