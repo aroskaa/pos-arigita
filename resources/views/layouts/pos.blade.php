0a3a91a3a9c6bc28e5782636e243f27d4f83c193
aroskaa <divaaroska10@gmail.com>
2026-06-13 10:42:01 +0800

    fix: webpage not loaded issue

--- resources/views/layouts/pos.blade.php
+++ resources/views/layouts/pos.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? 'POS Ari Gita Grosir' }}</title>
 
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
